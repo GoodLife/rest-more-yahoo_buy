d3d18e1168d5fde89da7bbdd45a4c4cd3c2cf868
lulalala <mark@goodlife.tw>
2013-06-04 10:21:51 +0800

    Change yahoo_buy class initialization so the the middleware under its namespace can be used.

--- lib/rest-core/client/yahoo_buy.rb
+++ lib/rest-core/client/yahoo_buy.rb
@@ -2,25 +2,26 @@
 require 'rest-core'
 
 module RestCore
-  YahooBuy = Builder.client do
-    use Timeout       , 10
+  module YahooBuy
+    autoload :Signature, 'rest-core/client/yahoo_buy/signature'
+    Client = Builder.client(:api_key) do
+      use Timeout       , 10
 
-    use DefaultSite   , 'http://tw.partner.buy.yahoo.com/api/v1/'
-    use DefaultHeaders, {'Accept' => 'application/xml'}
+      use Signature, nil
 
-    use CommonLogger  , nil
-    use Cache         , nil, 600 do
-      use ErrorHandler, lambda{ |env|
-        RuntimeError.new(env[RESPONSE_BODY]['message'])}
-      use ErrorDetectorHttp
+      use DefaultSite   , 'http://tw.partner.buy.yahoo.com/api/v1/'
+      use DefaultHeaders, {'Accept' => 'application/xml'}
+
+      use CommonLogger  , nil
+      use Cache         , nil, 600 do
+        use ErrorHandler, lambda{ |env|
+          RuntimeError.new(env[RESPONSE_BODY]['message'])}
+        use ErrorDetectorHttp
+      end
+    end
+
+    def self.new *args, &block
+      Client.new *args, &block
     end
   end
-end
-
-module RestCore::YahooBuy::Client
-  include RestCore
-end
-
-class RestCore::YahooBuy
-  include RestCore::YahooBuy::Client
 end
