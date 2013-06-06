f0cc722519d3f7f68407536028da0ce476b645ff
lulalala <mark@goodlife.tw>
2013-06-06 12:31:35 +0800

    Fix signature middleware: Use hexdigest to produce alphanumeric string. Yahoo API文件舉例錯誤，signature 這個 key 應全小寫

--- lib/rest-core/client/yahoo_buy/signature.rb
+++ lib/rest-core/client/yahoo_buy/signature.rb
@@ -1,18 +1,18 @@
 
 require 'rest-core/middleware'
-require 'rest-core/util/hmac'
+require 'openssl'
 
 class RestCore::YahooBuy::Signature
   def self.members; [:secret]; end
   include RestCore::Middleware
 
   def call env, &k
-    sig = Hmac.sha1(secret(env), percent_encode(env[REQUEST_QUERY]))
+    sig = OpenSSL::HMAC.hexdigest('sha1', secret(env), percent_encode(env[REQUEST_QUERY]))
 
     # Move query parameters into path variable, to stop reordering
     env[REQUEST_PATH] = request_uri(env)
     # Add signature to the end of request
-    env[REQUEST_QUERY] = {'Signature'=>sig}
+    env[REQUEST_QUERY] = {'signature'=>sig}
 
     app.call(env, &k)
   end
